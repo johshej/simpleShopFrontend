c8b8bf35d606ef2fce6d32b68894e8341ba0bb7d
Johannes Hejslet Jørgensen <johannes@hejslet.dk>
2018-07-22 02:16:48 +0200

    Fix currency format, though formatting is still redundnat

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -4,10 +4,10 @@
     <ul>
       <li v-for="product in products" v-bind:key="product.id">
         <div class="name">{{ product.name }} passed value: {{ testValue }}</div>
-        <div v-bind:class="{ selectedCurrency: currency == 'dkk', 'price': true }">{{ product.price_dkk }}<span class="uppercase">{{ currency }}</span></div>
-        <div v-bind:class="{ selectedCurrency: currency == 'sek', 'price': true }">{{ product.price_sek }}<span class="uppercase">{{ currency }}</span></div>
-        <div v-bind:class="{ selectedCurrency: currency == 'eur', 'price': true }">{{ product.price_eur }}<span class="uppercase">{{ currency }}</span></div>
-        <div v-bind:class="{ selectedCurrency: currency == 'usd', 'price': true }">{{ product.price_usd }}<span class="uppercase">{{ currency }}</span></div>
+        <div v-bind:class="{ selectedCurrency: currency == 'dkk', 'price': true }">{{ (product.price_dkk / 100).toFixed(2) }}<span class="uppercase">{{ currency }}</span></div>
+        <div v-bind:class="{ selectedCurrency: currency == 'sek', 'price': true }">{{ (product.price_sek / 100).toFixed(2) }}<span class="uppercase">{{ currency }}</span></div>
+        <div v-bind:class="{ selectedCurrency: currency == 'eur', 'price': true }">{{ (product.price_eur / 100).toFixed(2) }}<span class="uppercase">{{ currency }}</span></div>
+        <div v-bind:class="{ selectedCurrency: currency == 'usd', 'price': true }">{{ (product.price_usd / 100).toFixed(2) }}<span class="uppercase">{{ currency }}</span></div>
       </li>
     </ul>
   </div>
